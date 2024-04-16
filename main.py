d8eebe238860203cfd0897958cf9f9a191bbba6d
Gabriel Henrique Wetzel <gabrielhwetzel@gmail.com>
2024-04-16 10:18:11 -0300

    Created dummy Credit card function

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import pandas
 
 df = pandas.read_csv("hotels.csv", dtype={"id": str})
+df_cards = pandas.read_csv("cards.csv", dtype=str).to_dict(orient="records")
 
 
 class Hotel:
@@ -33,6 +34,19 @@ Hotel: {self.hotel_object.name}"""
         return content
 
 
+class CreditCard:
+    def __init__(self, number):
+        self.number = number
+
+    def validate(self, expiration, holder, cvc):
+        card_data = {"number": self.number,
+                     "expiration": expiration,
+                     "cvc": cvc,
+                     "holder": holder}
+        if card_data in df_cards:
+            return True
+
+
 print(df)
 hotel_id = input("Enter the id of the hotel: ")
 hotel = Hotel(hotel_id)
@@ -41,9 +55,13 @@ if hotel_id == "free all":
     df.loc[df['available'] == "no", "available"] = "yes"
     df.to_csv("hotels.csv", index=False)
 elif hotel.available():
-    hotel.book()
-    name = input("Enter your name: ")
-    reservation_ticket = ReservationTicket(name, hotel)
-    print(reservation_ticket.generate())
+    credit_card = CreditCard(number="1234567890123456")
+    if credit_card.validate(expiration="01/99", holder="JOHN DOE", cvc="000"):
+        hotel.book()
+        name = input("Enter your name: ")
+        reservation_ticket = ReservationTicket(name, hotel)
+        print(reservation_ticket.generate())
+    else:
+        print("There was a problem with your payment")
 else:
     print("Hotel is not available")
